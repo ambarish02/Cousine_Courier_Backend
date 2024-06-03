e9efbfbde2391473d755eea3012382fa8c6825e6
Ambarish Dandekar <156113775+ambarish02@users.noreply.github.com>
2024-06-03 17:44:45 +0530

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -11,7 +11,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 const placeOrder = async (req, res) => {
    
         
-const frontend_url = "http://localhost:5173"    
+const frontend_url = "https://cuisine-courier.vercel.app"    
     try {
         const newOrder = new orderModel({
             userId: req.body.userId,
